031f3852ef36bc5f16e4ee5dc0f8bf32c3462844
Johanth P S <johanth6600@gmail.com>
2023-06-29 15:08:54 +0530

    initial commit from dev machine On branch master

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/johanth/waymaster_v1/src
+# Build directory: /home/johanth/waymaster_v1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("freenect_stack/freenect_launch")
+subdirs("freenect_stack/freenect_stack")
+subdirs("freenect_stack/freenect_camera")
+subdirs("waymaster_v1")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/johanth/waymaster_v1/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/johanth/waymaster_v1/devel;/home/johanth/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/johanth/waymaster_v1/devel;/home/johanth/catkin_ws/devel;/opt/ros/noetic' != '' else []
