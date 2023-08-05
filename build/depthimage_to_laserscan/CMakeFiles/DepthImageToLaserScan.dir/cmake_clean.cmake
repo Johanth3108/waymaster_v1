5ac6bd4dabf2ad1de1655ce6627903f43818a0e9
Johanth P S <johanth6600@gmail.com>
2023-08-05 11:23:25 +0530

    changing OS from 18 to 14

---
+++ build/depthimage_to_laserscan/CMakeFiles/DepthImageToLaserScan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/johanth/waymaster_v1/devel/lib/libDepthImageToLaserScan.pdb"
+  "/home/johanth/waymaster_v1/devel/lib/libDepthImageToLaserScan.so"
+  "CMakeFiles/DepthImageToLaserScan.dir/src/DepthImageToLaserScan.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DepthImageToLaserScan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
